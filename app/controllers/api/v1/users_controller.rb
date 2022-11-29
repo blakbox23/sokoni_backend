4ebf3b0f127f1c5c4aefb2826b600cb8851a6da8
blakbox23 <boudii30@gmail.com>
2022-11-29 08:05:07 +0300

    Add username column in users table

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -29,10 +29,7 @@
       private
       
       def user_params
-        params.require(:user).permit(:email, :password, :password_confirmation)
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
       end
-
-
-
 
 end
