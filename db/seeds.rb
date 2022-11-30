a461f7d385579de23c370a85939ca133300f4323
blakbox23 <boudii30@gmail.com>
2022-11-30 21:40:15 +0300

    set up for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,73 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.destroy_all
+
+Product.create!([{
+    name: "Pineaple",
+    price: 200,
+    imgUrl: "/imgs/pineaples.png",
+    productQuant: "5 pcs",
+    category": "Fruits"
+},
+{
+    name: "Avocados",
+    price: 50,
+    imgUrl: "/imgs/avocados.png",
+    productQuant: "5 pcs",
+    category": "Fruits"
+},
+{
+    name: "Sweet potatoes",
+    price: 100,
+    imgUrl: "/imgs/potatoes.png",
+    productQuant: "1 Kg",
+    category": "Vegetables"
+},
+{
+    name: "Bananas",
+    price: 50,
+    imgUrl: "/imgs/bananas.png",
+    productQuant: "1 Kg",
+    category": "Fruits"
+},
+{
+    name: "Broccoli",
+    price: 100,
+    imgUrl: "/imgs/broccoli.png",
+    productQuant: "1 Kg",
+    category": "Vegetables"
+},
+{
+    name: "Spinach",
+    price: 100,
+    imgUrl: "/imgs/spinach.png",
+    productQuant: "1 Kg",
+    category": "Vegetables"
+},
+{
+    name: "Carrots",
+    price: 100,
+    imgUrl: "/imgs/carrots.png",
+    productQuant: "1 Kg",
+    category": "Vegetables"
+},
+{
+    name: "Milk",
+    price: 70,
+    imgUrl: "/imgs/milk.png",
+    productQuant: "1 Litre",
+    category": "Dairy"
+},
+{
+    name: "Cabbage",
+    price: 50,
+    imgUrl: "/imgs/cabbage.png",
+    productQuant: "1 Litre",
+    category": "Vegetables"
+},
+
+
+])
+
+p "Created #{Product.count} products"
